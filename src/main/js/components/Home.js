3c60fda9d1d125907bd236b415c363b577bf5834
Denis Komyagin <blackpirates@mail.ru>
2018-02-08 23:09:39 +0300

    HomePage

--- src/main/js/components/Home.js
+++ src/main/js/components/Home.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react'
+import React, {Component} from "react";
 
 export default class Home extends Component {
     render() {
         return (
-            <div className="test">
-                Стартовая страница
-            </div>
+            <div className="test"></div>
         )
     }
 }
